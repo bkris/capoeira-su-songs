26f1b3598fb25a4f6d2e1d77a6395d4ebae51a38
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-17 15:36:18 +0200

    Added more songs

--- src/songs/index.js
+++ src/songs/index.js
@@ -2,9 +2,12 @@ import song1 from './capoeira pra me e uma so.json';
 import song2 from './a capoeira tem vida.json';
 import song3 from './saudades do meu maranhao.json';
 import song4 from './beira mar beira mar novo.json';
+import song5 from './berimbau chamou voce.json';
+import song6 from './marinheiro so.json';
+import song7 from './vim no navio de aruanda aruanda eee.json';
 
 const songs = [
-  song1, song2, song3, song4
+  song1, song2, song3, song4, song5, song6, song7
 ];
 
 export default songs;
