b49b476277dc3f8c457ebf32fe4e375c0ad569cb
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-17 15:03:39 +0200

    Refactored Media component

--- src/page/Media.js
+++ src/page/Media.js
@@ -1,19 +1,34 @@
 import {Ratio} from "react-bootstrap";
+import {isEmpty} from "lodash";
+
+const NO_MEDIA = <></>;
+
+function YoutubeVideo({link}) {
+  const parsedUrl = new URL(link);
+  const videoId = parsedUrl.searchParams.get('v')
+
+  if (isEmpty(videoId)) {
+    return NO_MEDIA;
+  }
+
+  const embedUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+
+  return (
+    <Ratio aspectRatio="16x9">
+      <iframe src={embedUrl} allowFullScreen></iframe>
+    </Ratio>
+  )
+}
 
 function Media({link, provider, type}) {
   if (type !== "video") {
-    return <div></div>;
+    return NO_MEDIA
   }
 
   if (provider === 'youtube') {
-    const videoId = link.split('v=')[1];
-    const embedUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
-
-    return (
-      <Ratio aspectRatio="16x9">
-        <iframe src={embedUrl} allowFullScreen></iframe>
-      </Ratio>
-    )
+    return <YoutubeVideo link={link}/>
   }
+
+  return NO_MEDIA;
 }
 export default  Media;
