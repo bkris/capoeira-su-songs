f29dfdb889327bc119b6c61c51f1cf273a549235
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-17 17:33:17 +0200

    Table Added more songs

--- src/songs/index.js
+++ src/songs/index.js
@@ -6,9 +6,16 @@ import song5 from './berimbau chamou voce.json';
 import song6 from './marinheiro so.json';
 import song7 from './vim no navio de aruanda aruanda eee.json';
 import song8 from './ai ai aide.json';
+import song9 from './mandei caia meu sobrado.json';
+import song10 from './foi na bahia que eu mandei fazer.json';
+import song11 from './oi na praca se que a policia me parou.json';
+import song12 from './certo dia na cabana um guerreiro.json';
+import song13 from './o mundo de deus e grande.json';
+import song14 from './minha varanda, contramestre monkey.json';
+import song15 from './eu sou da geracao depois de bimba.json';
 
 const songs = [
-  song1, song2, song3, song4, song5, song6, song7, song8
+  song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11, song12, song13, song14, song15
 ];
 
 export default songs;
