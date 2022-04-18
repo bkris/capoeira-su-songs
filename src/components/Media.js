ad5a664f6f72c6b8add8ccc84e171c0350245986
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-18 10:20:55 +0200

    Added support for embedding fb videos

--- src/components/Media.js
+++ src/components/Media.js
@@ -20,6 +20,20 @@ function YoutubeVideo({link}) {
   )
 }
 
+function FacebookVideo({link}) {
+  const urlEncodedLink = encodeURI(link);
+  const fullLink = `https://www.facebook.com/plugins/video.php?href=${urlEncodedLink}&show_text=0&width=560`
+
+  return (
+    <Ratio>
+      <iframe
+        src={fullLink}
+        width="560" height="420" style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder="0"
+        allowFullScreen allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"/>
+    </Ratio>
+  )
+}
+
 function Media({link, provider, type}) {
   if (type !== "video") {
     return NO_MEDIA
@@ -27,6 +41,10 @@ function Media({link, provider, type}) {
 
   if (provider === 'youtube') {
     return <YoutubeVideo link={link}/>
+  }
+
+  if (provider === 'facebook') {
+    return <FacebookVideo link={link}/>
   }
 
   return NO_MEDIA;
