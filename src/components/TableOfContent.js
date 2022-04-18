f82ff8cabca1317e840b6a2c4bd73387bedef9ad
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-18 16:03:06 +0200

    Indicating song type in table of content

--- src/components/TableOfContent.js
+++ src/components/TableOfContent.js
@@ -5,8 +5,8 @@ function TableOfContent({children , songs = []}) {
   const songList = songs.map((song, index) => {
     const id = `#${slugify(song.name)}`;
     return (
-      <ListGroup.Item key={index} action href={id}>
-        {index + 1}. {song.name}
+      <ListGroup.Item key={index} action href={id} className="d-flex justify-content-between">
+        <span>{index + 1}. {song.name}</span>{song.type && <span>{song.type}</span>}
       </ListGroup.Item>
     )
   })
