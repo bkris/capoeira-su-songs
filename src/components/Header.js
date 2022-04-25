385ebb0c6e3fde1683936cf3ffe41a39b9ed5cc3
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-25 21:24:22 +0200

    Revert "Added logo to site"
    This reverts commit a56e5ea4aa00c0a4031ab6cf17e261baa8d80056.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,7 @@ import {Row, Col} from "react-bootstrap";
 function Header() {
   return (
     <Row className="mb-5">
-      <Col md={3}>
-        <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="Capoeira Subotica"/>
-      </Col>
-      <Col md={9}>
+      <Col md={8}>
         <h2>Capoeira songs</h2>
         <p>Songs we practice at Capoeira Subotica group</p>
         <p>Most of the translations are auto translated with google translate. We do not keep responsibility of the
