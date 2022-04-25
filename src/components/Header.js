a56e5ea4aa00c0a4031ab6cf17e261baa8d80056
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-25 20:13:25 +0200

    Added logo to site

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import {Row, Col} from "react-bootstrap";
 function Header() {
   return (
     <Row className="mb-5">
-      <Col md={8}>
+      <Col md={3}>
+        <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="Capoeira Subotica"/>
+      </Col>
+      <Col md={9}>
         <h2>Capoeira songs</h2>
         <p>Songs we practice at Capoeira Subotica group</p>
         <p>Most of the translations are auto translated with google translate. We do not keep responsibility of the
