006ce37d1b5fe47d67a788cbbe03ba2d6b334c52
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-04-26 06:36:42 +0200

    Added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,14 @@ import {Row, Col} from "react-bootstrap";
 function Header() {
   return (
     <Row className="mb-5">
-      <Col md={8}>
-        <h2>Capoeira songs</h2>
-        <p>Songs we practice at Capoeira Subotica group</p>
-        <p>Most of the translations are auto translated with google translate. We do not keep responsibility of the
-          accurate translation nor the ethical correctness.</p>
+      <Col md={8} className="d-flex ">
+        <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="Capoeira Subotica" className="logo"/>
+        <div>
+          <h2>Capoeira songs</h2>
+          <p>Songs we practice at Capoeira Subotica group</p>
+          <p>Most of the translations are auto translated with google translate. We do not keep responsibility of the
+            accurate translation nor the ethical correctness.</p>
+        </div>
       </Col>
     </Row>
   );
