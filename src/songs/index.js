d7d0a24275e750131269990201b4e215c265a60e
Kristijan Bognar <kristijan.bognar@gmail.com>
2022-12-27 10:24:09 +0100

    Added more song

--- src/songs/index.js
+++ src/songs/index.js
@@ -58,13 +58,16 @@ import song57 from './dono das tres estrelas.json';
 import song58 from './mandei fazer meu terreiro.json';
 import song59 from './vou ver juliana.json';
 import song60 from './e crianca quero brincar com voce.json';
+import song61 from './meu camarada eu vim botar dende.json';
+import song62 from './o que e berimbau.json';
 // import song60 from './';
 
 const songs = [
   song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11, song12, song13, song14, song15, song16,
   song17, song18, song19, song20, song21, song22, song23, song24, song25, song26, song27, song28, song29, song30, song31,
   song32, song33, song34, song35, song36, song37, song38, song39, song40, song41, song42, song43, song44, song45, song46,
-  song47, song48, song49, song50, song51, song52, song53, song54, song55, song56, song57, song58, song59, song60
+  song47, song48, song49, song50, song51, song52, song53, song54, song55, song56, song57, song58, song59, song60, song61,
+  song62
 ];
 
 export default songs;
