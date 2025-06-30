f250805e9a9387ef998cd835d53e64d440c41652
Kristijan Bognar <kristijan.bognar@gmail.com>
2025-06-30 16:31:30 +0200

    Added more song

--- src/songs/index.js
+++ src/songs/index.js
@@ -80,6 +80,7 @@ import song79 from './e de ioio.json';
 import song80 from './vamos vadiar angola.json';
 import song81 from './sabia cantou.json';
 import song82 from './parana e parana.json';
+import song83 from './vadiar no aais Joao grilo.json';
 
 // import song80 from './';
 
@@ -89,7 +90,7 @@ const songs = [
   song32, song33, song34, song35, song36, song37, song38, song39, song40, song41, song42, song43, song44, song45, song46,
   song47, song48, song49, song50, song51, song52, song53, song54, song55, song56, song57, song58, song59, song60, song61,
   song62, song63, song64, song65, song66, song67, song68, song69, song70, song71, song72, song73, song74, song75, song76,
-  song77, song78, song79, song80, song81, song82
+  song77, song78, song79, song80, song81, song82, song83
 ];
 
 export default songs;
